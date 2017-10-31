f9ff0c5faaf3c8a8ca6b1b0d922d289b7a0fccd0
ariefnurputranto <arief@qiscus.co>
2017-10-31 15:19:44 +0700

    change text default placeholder

--- Qiscus/Qiscus/Configuration/QiscusTextConfiguration.swift
+++ Qiscus/Qiscus/Configuration/QiscusTextConfiguration.swift
@@ -27,7 +27,7 @@ open class QiscusTextConfiguration: NSObject {
     open var readOnlyText = "Archieved message: This message was locked. Click the key to open the conversation."
     
     /// Your text placeholder if you want to send any message, Default value : "Text a message here ..."
-    open var textPlaceholder = "Text Message"
+    open var textPlaceholder = "Enter Message"
     
     public var captionPlaceholder = "Caption here..."
     /// Your text to show as title alert when you access gallery but you not allow gallery access, Default value : "Important"
